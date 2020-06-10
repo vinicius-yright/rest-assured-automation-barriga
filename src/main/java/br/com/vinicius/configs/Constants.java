3a8397660ed8fcda442f581a5db248b79d43e5cc
vinicius.brito <vsdbpessoal@gmail.com>
2020-06-10 11:36:26 -0300

    10-06 completo

---
+++ src/main/java/br/com/vinicius/configs/Constants.java
@@ -0,0 +1,13 @@
+package br.com.vinicius.configs;
+
+import io.restassured.http.ContentType;
+
+public interface Constants {
+
+	String APP_BASE_URL = "http://barrigarest.wcaquino.me/";
+	String APP_BASE_PATH = "";
+	
+	ContentType APP_CONTENT_TYPE = ContentType.JSON;
+	
+	
+}
